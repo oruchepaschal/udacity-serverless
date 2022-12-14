76b7da0d37deda173d5466ed606fec1f103e83be
“oruchepaschal” <oruchepaschal@gmail.com>
2022-12-14 14:18:37 +0100

    fix branches

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,11 +1,12 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'yphnttxi1l'
+const apiId = 'zhoj3fftr8'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
   domain: 'captchacoder.us.auth0.com',            // Auth0 domain
-  clientId: 'eokg2TmZnZUyFtw2RDKcScHYkRxKdxkM',          // Auth0 client id
+  clientId: '754sUyLU3OSlZsMhXctXT0fXOykl3mmW',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
+//https://zhoj3fftr8.execute-api.us-east-1.amazonaws.com/dev
